e90b3b359513620e7ccd8c50074f499062d6d166
Rey Bonan <marcreybonan@gmail.com>
2024-10-13 03:34:07 +0200

    Create bases.rkt

---
+++ Racket/bases.rkt
@@ -0,0 +1,20 @@
+#lang racket ; On définit le langage
+; Pour écrire un commentaire
+
+; On peut également écrire un bloc de commentaires sur plusieurs lignes 
+
+ #| Ne pas confondre / et | cependant
+    passons au début |#
+
+; Bases des types de données 
+
+1 ; entier
+1.1 ; réel et flottant
+1/1 ; exact
+1+11i ; complexe
+
+#t ; booléen true
+#f ; booléen false
+; Toute autre valeur que #f est vraie
+
+
