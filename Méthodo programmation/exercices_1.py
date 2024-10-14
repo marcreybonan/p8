5637bd2eaa62b1b58d81b423ac20323715e4d88e
Rey Bonan <marcreybonan@gmail.com>
2024-10-14 21:22:46 +0200

    Update exercices_1.py

--- Méthodo programmation/exercices_1.py
+++ Méthodo programmation/exercices_1.py
@@ -1,1 +1,10 @@
+#Creation d'une liste simple 
+#On crée une liste simple qu'on affichera ensuite avant d'afficher un de ses éléments particulier
 
+liste = ["L1", "L2", "L3", "M1", "M2"]
+print(liste)
+
+#J'affiche un élément de ma liste décrivant ma situation 
+print = ("Je suis en" liste[0])
+
+#Accès aux éléments d'une liste
