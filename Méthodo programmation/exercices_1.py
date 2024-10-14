c4febba9f9a234feefcf9a2f43835f14e5c65cb3
Rey Bonan <marcreybonan@gmail.com>
2024-10-15 00:02:56 +0200

    Update exercices_1.py

--- Méthodo programmation/exercices_1.py
+++ Méthodo programmation/exercices_1.py
@@ -1,3 +1,4 @@
+#EXERCICE 1
 #Creation d'une liste simple 
 #On crée une liste simple qu'on affichera ensuite avant d'afficher un de ses éléments particulier
 
@@ -7,4 +8,41 @@ print(liste)
 #J'affiche un élément de ma liste décrivant ma situation 
 print = ("Je suis en" liste[0])
 
-#Accès aux éléments d'une liste
+#EXERCICE 2
+#Accès au contenu d'une liste
+#On va créer une liste avec 6 éléments, chaque élément est un organe
+
+listte = ["cerveau", "reins", "coeur", "foie", "poumons", "thyroide"]
+
+#On demande à 3 étudiants en médecine lequel il préfère
+
+print("Je préfère le", listte[0])  
+print("Moi je préfère la", listte[5])
+print("Je ne vous comprends pas car mon organe favori est le", listte[2])
+
+#EXERCICE 3
+#Modifier le contenu d'une liste en supprimant le deuxième élément de cette liste et en rajoutant un dernier élément
+
+ma_liste = ["Un", "Deux", "Trois", "Quatre"]
+ma_liste.remove("Deux")
+ma_liste.append("Cinq") 
+
+#On affiche le contenu de la liste avant de la supprimer
+
+print(ma_liste)
+ma_liste.clear()
+
+#EXERCICE 4
+#Ajouter un nouvel élément à la fin d'une liste puis supprimer le premier élément
+
+lst = ["1", "2", "3"]
+lst.append("4")
+lst.remove("1")
+
+#S'inspirer de l'exercice 11 pouur mes élèves
+
+
+
+
+
+
