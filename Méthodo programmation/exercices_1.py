ce207b6eb0b170e8abe0e4db9c7cb8cc534319f0
Rey Bonan <marcreybonan@gmail.com>
2024-10-16 17:11:17 +0200

    Update exercices_1.py

--- Méthodo programmation/exercices_1.py
+++ Méthodo programmation/exercices_1.py
@@ -42,6 +42,56 @@ lst.remove("1")
 #S'inspirer de l'exercice 11 pouur mes élèves
 
 
+#EXERCICE 5
+
+#On demande à nos élèves de saisir des nombres (pas trop non plus) afin d'en faire ensuite une liste
+input_liste = input("Saisissez des nombres mais séparés d'un espace : ")
+
+# Diviser la chaîne d'entrée et convertir chaque élément en entier  
+#split() nous est utile afin de construire une liste et ce en exploitant les espaces laissés volontairement
+liste_complete = input_liste.split()
+
+# Afficher la liste  
+print(liste_complete)
+
+#Annoncer que l'on va inverser la liste
+print("Cette liste sera inversée")
+
+#Inverser les éléments de la liste avec reverse() et vérifier le résultat en affichant liste_complete
+liste_complete.reverse()
+print(liste_complete)
+
+#On pourrait félicier ou non les élèves si le résultat est bon...
+
+
+#EXERCICE 6  
+
+#Solution de facilité, si un nombre est pair alors le reste de la division par 2 sera nul 
+#Ainsi il est choisi d'utiliser le modulo % et, considérons une liste [x, y, z] si x,y,z%2 == 0 alors ces nombres seront pairs et il n'y aura plus qu'à les afficher 
+#Ainsi on considère une liste quelconque
+
+liste = [3, 45, 32, 54, 37, 5432, 9374, 2938, 28475]
+
+for nombres in liste:
+  if nombres%2 == 0:
+    print("voici les nb paires", nombres)
+    
+#On pourrait écrire les nb n'étant pas paires via else 
+
+#EXERCICE 7 
+
+#On pourrait utiliser l'opérateur sum() mais il est demandé d'utiliser l'opérateur for
+
+somme = 0
+liste = [2, 3, 43, 56, 33]
+
+#Après avoir initialisé la variable somme et avoir crée notre liste nous n'avons plus qu'à calculer la somme et l'afficher
+for nb in liste:
+  somme+=nb
+print("La somme est de" somme)
+
+
+
 
 
 
