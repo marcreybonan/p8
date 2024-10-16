3fbd330e5991a6748c124c50019014bd49917b1f
Rey Bonan <marcreybonan@gmail.com>
2024-10-16 18:35:19 +0200

    Update exercice1_part2.py

--- Méthodo programmation/exercice1_part2.py
+++ Méthodo programmation/exercice1_part2.py
@@ -10,6 +10,7 @@ for ix, donnee in enumerate(tuple):
      print(f"{ix} : {donnee}")
 
 #EXERCICE 2
+
 #Impossible de changer un éléménent des tuples du fait de leur caractère immutable
 #Cette différence avec les listes notamment a plusieurs avantages
 #Moins de bug ?
@@ -37,3 +38,32 @@ dictionnaire_notes.pop(etudiante_partante)
  
 # On affiche le dictionnaire avec les étudiants restants 
 print("Eleves restants:", dictionnaire)
+
+
+#EXERCICE 5
+
+#Création de notre liste  
+amis = (("Marco", 27), ("Andra", 28), ("Bugsy", 28), ("Alexis", 28), ("Gaby", 10), ("Polo", 29))
+#Gaby a en fait 30 ans 
+
+#On affiche les personnes dont l'age est sup à 20 ans
+print("Qui a plus de 20 ans ? :")
+for prenom, age in amis:
+    if age > 20:
+        print(prenom)
+
+EXERCICE 6
+
+#Créations des listes 
+gundam = [("Providence"),("Duel"),("Legend"),("Astray")]
+prix = [(30), (20), (25), (15)]
+
+#Fusionner les listes à l'aide de zip puis affichage de la liste
+liste_unique = list(zip(gundam, prix))
+print("liste unique :")
+for gundam, prix in liste_fusionnee:
+    print(f"{gundam[0]} : {prix[0]}")
+
+#Sympa comme exercice
+
+
